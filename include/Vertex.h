b396fa9c12870562c1371d6245f292321250a3cc
anasxyz <anaswessam176@gmail.com>
2025-11-19 15:20:48 +0000

    fix Vertex struct memory offset issue

--- include/Vertex.h
+++ include/Vertex.h
@@ -11,12 +11,12 @@ struct Vertex {
   vec3 normal;
   vec2 uv;
 
-  Vertex() : position(0.0f), colour(1.0f), normal(0.0f, 1.0f, 0.0f), uv(0.0f) {}
-
+  Vertex(glm::vec3 p, glm::vec3 n, glm::vec2 t)
+      : position(p), colour(1.0f, 1.0f, 1.0f, 1.0f), normal(n), uv(t) {}
 };
 
 /*
- * idk why the render bug is happening
+ * idk why the render bug is happening.
  *
  * position = 12 bytes because vec3
  * colour = 16 bytes because vec4
@@ -24,6 +24,13 @@ struct Vertex {
  * uv = 8 bytes because vec2
  *
  * total = 48 bytes
+ *
+ * okay I know the cause now, when I define vec4 colour under
+ * vec3 position the bug happens, when I define it under vec2 uv (all
+ * the way at the bottom) it works fine, but I dont know why.
+ *
+ * Suddenly it doesn't matter where I define colour anymore, it just
+ * works.
  *
  */
 static_assert(sizeof(Vertex) == 48, "Vertex struct must be tightly packed");
